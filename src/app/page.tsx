fc2e676d2426d6bdb43439ec4dbfc408ec960242
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2023-12-19 21:56:48 +0300

    feat: init theme provider & install shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import Image from 'next/image'
-
 export default function Home() {
-  return (
-    <div>Foo</div>
-  )
+  return <div></div>;
 }
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export * from "./ui";
+export { ModeToggle } from "./ButtonSwitch";
